7ece265ea5365a87d915f65db1e175f0524ffea0
Alexandra Dmitrieva <aallycoop@gmail.com>
2023-02-08 22:11:01 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,9 @@ const program = new Command();
 program
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0', '-V, --version', 'output the version number')
-    .helpOption('-h, --help', 'display help for command');
+    .helpOption('-h, --help', 'display help for command')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format <type>', 'output format');
 
 program.parse();
