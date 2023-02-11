e31b41cd487d4ff6b3b636ca975b548c7743d39a
Alexandra Dmitrieva <aallycoop@gmail.com>
2023-02-11 18:25:06 +0300

    step 4: add tests for gendiff between json - json

---
+++ __tests__/index.tests.js
@@ -0,0 +1,15 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+// import path from 'path';
+import { readFileSync } from 'node:fs';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+test('gendiff between json - json', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('expected_file.txt'));
+});
