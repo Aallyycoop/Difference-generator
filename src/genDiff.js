3627b821b6b6c3cea8e481e4446adda0c36d6837
Alexandra Dmitrieva <aallycoop@gmail.com>
2023-02-13 21:28:10 +0300

    step 6: add function for AStree

---
+++ src/genDiff.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+
+const genDiff = (object1, object2) => {
+  const keys = _.union(_.keys(object1), _.keys(object2));
+  const sortedKeys = _.sortBy(keys);
+  // console.log(sortedKeys);
+  const differenceTree = sortedKeys.map((key) => {
+    if (!_.has(object1, key)) {
+      return { key, type: 'added', value: object2[key] };
+    }
+    if (!_.has(object2, key)) {
+      return { key, type: 'deleted', value: object1[key] };
+    }
+    if (_.isObject(object1[key]) && _.isObject(object2[key])) {
+      return { key, type: 'nested', children: genDiff(object1[key], object2[key]) };
+    }
+    if (object1[key] !== object2[key]) {
+      return {
+        key, type: 'changed', value1: object1[key], value2: object2[key],
+      };
+    }
+    return { key, type: 'unchanged', value1: object1[key] };
+  });
+  // console.log(differenceTree);
+  return differenceTree;
+};
+
+export default genDiff;
