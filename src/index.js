17dcd7beb2e5c5df65a5f9306cb64f3fb6215fbe
Alexandra Dmitrieva <aallycoop@gmail.com>
2023-02-16 12:21:30 +0300

    step 6: update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,35 @@ import { readFileSync } from 'node:fs';
 import _ from 'lodash';
 import path from 'path';
 import parser from './parsers.js';
+import genDiff from './genDiff.js';
+import stylish from './stylish.js';
 
 const getFormat = (filepath) => path.extname(filepath);
+const readFile = (filepath) => readFileSync(path.resolve(process.cwd(), filepath), 'utf-8');
 
 // const getFormat = path.extname('__fixtures__/file1.json');
 // console.log(getFormat);
 
 export default (filepath1, filepath2) => {
+  const object1 = parser(readFile(filepath1), getFormat(filepath1));
+  const object2 = parser(readFile(filepath2), getFormat(filepath2));
+  const diffTree = genDiff(object1, object2);
+  // console.log(diffTree);
+  // return diffTree;
+  return stylish(diffTree);
+};
+
+// export default getDiff;
+
+
+// const path1 = '../__fixtures__/file1.json';
+// const path2 = '../__fixtures__/file2.json';
+
+// console.log(parser(readFile(path1), getFormat(path1)));
+
+// getDiff(path1, path2);
+
+/* export default (filepath1, filepath2) => {
   const data1 = readFileSync(path.resolve(process.cwd(), filepath1), 'utf-8');
   const data2 = readFileSync(path.resolve(process.cwd(), filepath2), 'utf-8');
   // console.log(process.cwd());
@@ -37,19 +59,10 @@ export default (filepath1, filepath2) => {
     return [...acc, `  + ${key}: ${dataParse2[key]}`];
   }, []);
   return ['{', ...result, '}'].join('\n');
-};
+}; */
 
 // gendiff __fixtures__/file1.json __fixtures__/file2.json
 
 // gendiff __fixtures__/file3.yaml __fixtures__/file4.yml
 
 // gendiff file1.json file2.json
-
-/* {
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-} */
