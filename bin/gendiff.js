fe4d6f2deca40911790325e7a3baf19c655c96d1
Alexandra Dmitrieva <aallycoop@gmail.com>
2023-02-16 12:19:46 +0300

    step 6: update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,8 +12,8 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .action((filepath1, filepath2) => {
-    const option = program.opts();
+  .action((filepath1, filepath2, option) => {
+    // const option = program.opts();
     console.log(genDiff(filepath1, filepath2, option.format));
   });
 
