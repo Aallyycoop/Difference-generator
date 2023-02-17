c86574e64d6c2857f0a22305d7b93beb2c6b827b
Alexandra Dmitrieva <aallycoop@gmail.com>
2023-02-17 19:10:20 +0300

    step 7: update fixtures, update tests

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -10,10 +10,18 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff between json - json', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('expected_file.txt'));
+test('gendiff between json - json, stylish format', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('stylish_expected_file.txt'));
 });
 
-test('gendiff between yaml - yml', () => {
-  expect(genDiff(getFixturePath('file3.yaml'), getFixturePath('file4.yml'))).toEqual(readFile('expected_file.txt'));
+test('gendiff between yaml - yml, stylish format', () => {
+  expect(genDiff(getFixturePath('file3.yaml'), getFixturePath('file4.yml'))).toEqual(readFile('stylish_expected_file.txt'));
+});
+
+test('gendiff between json - json, plain format', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('plain_expected_file.txt'));
+});
+
+test('gendiff between yaml - yml, plain format', () => {
+  expect(genDiff(getFixturePath('file3.yaml'), getFixturePath('file4.yml'))).toEqual(readFile('plain_expected_file.txt'));
 });
