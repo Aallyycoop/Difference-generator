5f3f5bdce7088a625f922d5c0ac8fc144adca4e0
Alexandra Dmitrieva <aallycoop@gmail.com>
2023-02-18 21:00:13 +0300

    fix cognitive complexity of plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -11,19 +11,17 @@ const plain = (differenceTree) => {
   const iter = (differenceNodes, path) => {
     const result = differenceNodes.map((node) => {
       const currentPath = `${path}${node.key}.`;
-      if (node.type === 'nested') {
-        return iter(node.children, currentPath);
+      switch (node.type) {
+        case 'nested':
+          return iter(node.children, currentPath);
+        case 'added':
+          return `Property '${currentPath.slice(0, -1)}' was added with value: ${stringify(node.value)}`;
+        case 'deleted':
+          return `Property '${currentPath.slice(0, -1)}' was removed`;
+        case 'changed':
+          return `Property '${currentPath.slice(0, -1)}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
+        default: return '';
       }
-      if (node.type === 'added') {
-        return `Property '${currentPath.slice(0, -1)}' was added with value: ${stringify(node.value)}`;
-      }
-      if (node.type === 'deleted') {
-        return `Property '${currentPath.slice(0, -1)}' was removed`;
-      }
-      if (node.type === 'changed') {
-        return `Property '${currentPath.slice(0, -1)}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
-      }
-      return '';
     });
     return result.filter((line) => line !== '').join('\n');
   };
