c6c06576760f033814430d07a3a8658fbc2fd29b
Alexandra Dmitrieva <aallycoop@gmail.com>
2023-02-18 13:46:43 +0300

    update main function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { readFileSync } from 'node:fs';
 import path from 'path';
 import parser from './parsers.js';
 import genDiff from './genDiff.js';
-import stylish from './stylish.js';
+// import stylish from './stylish.js';
+// import plain from './plain.js';
+import getFormatter from '../formatters/index.js';
 
 const getFormat = (filepath) => path.extname(filepath);
 const readFile = (filepath) => readFileSync(path.resolve(process.cwd(), filepath), 'utf-8');
@@ -11,13 +13,14 @@ const readFile = (filepath) => readFileSync(path.resolve(process.cwd(), filepath
 // const getFormat = path.extname('__fixtures__/file1.json');
 // console.log(getFormat);
 
-export default (filepath1, filepath2) => {
+export default (filepath1, filepath2, formatName = 'stylish') => {
   const object1 = parser(readFile(filepath1), getFormat(filepath1));
   const object2 = parser(readFile(filepath2), getFormat(filepath2));
   const diffTree = genDiff(object1, object2);
   // console.log(diffTree);
   // return diffTree;
-  return stylish(diffTree);
+  // return stylish(diffTree);
+  return getFormatter(diffTree, formatName);
 };
 
 // export default getDiff;
